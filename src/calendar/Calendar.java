280d05c9ae254e6d20c3fb7ed8bcf1f0243b6c98
wkqk <brew_m@naver.com>
2017-08-27 18:44:48 +0900

    4번째-한글 변경 적용

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,13 +3,14 @@ package calendar;
 public class Calendar {
 
 	public static void main(String[] args) {
-		System.out.println("ÀÏ     ¿ù      È­      ¼ö      ¸ñ      ±İ     Åä");
+		System.out.println(" ì¼     ì›”      í™”       ìˆ˜      ëª©      ê¸ˆ     í† ");
 		System.out.println("___________________________");
 		System.out.println(" 1   2   3   4   5   6   7");
 		System.out.println(" 8   9  10  11  12  13  14");
 		System.out.println("15  16  17  18  19  20  21");
 		System.out.println("23  23  24  25  26  27  28");
 		System.out.println("29  30  31");
+		System.out.println();
 	}
 
 }
