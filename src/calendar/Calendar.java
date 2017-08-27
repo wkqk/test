96e2d58c1a212c90fafd044e568567048059e4c8
wkqk <brew_m@naver.com>
2017-08-27 18:36:40 +0900

    third....

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,9 +3,13 @@ package calendar;
 public class Calendar {
 
 	public static void main(String[] args) {
-		System.out.println("Hi, Calendar....");
-		System.out.println("second Editing....");
-		System.out.println("second Editing...................");
+		System.out.println("일     월      화      수      목      금     토");
+		System.out.println("___________________________");
+		System.out.println(" 1   2   3   4   5   6   7");
+		System.out.println(" 8   9  10  11  12  13  14");
+		System.out.println("15  16  17  18  19  20  21");
+		System.out.println("23  23  24  25  26  27  28");
+		System.out.println("29  30  31");
 	}
 
 }
